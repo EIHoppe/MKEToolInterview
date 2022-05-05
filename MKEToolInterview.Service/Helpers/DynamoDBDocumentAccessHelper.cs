4824ae20f9c9fd5c17a9986a7bc02bd5e55c2ddc
Eric Hoppe <eihoppe@gmail.com>
2022-05-05 11:36:37 -0500

    Add access helper for dynamo docs

---
+++ MKEToolInterview.Service/Helpers/DynamoDBDocumentAccessHelper.cs
@@ -0,0 +1,28 @@
+﻿using Amazon.DynamoDBv2.DocumentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MKEToolInterview.Service.Helpers
+{
+    public class DynamoDBDocumentAccessHelper
+    {
+        public static string AccessDocumentAttribute(Document document, string attributeName)
+        {
+            var attributes = document.GetAttributeNames();
+            string returnedString = null;
+
+            if (attributes.Contains(attributeName))
+            {
+                var value = document[attributeName];
+                if (value is Primitive)
+                {
+                    returnedString = value.AsPrimitive().Value.ToString();
+                }
+                // TODO: add list handling (out of scope for first pass)
+            }
+
+            return returnedString;
+        }
+    }
+}
