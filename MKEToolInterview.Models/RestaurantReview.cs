7935da7f7f6f4177c8b77df1b43336ae27d5aad8
Eric Hoppe <eihoppe@gmail.com>
2022-05-05 12:38:35 -0500

    Add comments to models / fix IDs for reviews

--- MKEToolInterview.Models/RestaurantReview.cs
+++ MKEToolInterview.Models/RestaurantReview.cs
@@ -7,9 +7,32 @@ namespace MKEToolInterview.Models
 {
     public class RestaurantReview
     {
+
+        /// <summary>
+        /// The ID of the restaurant this review is associated with
+        /// </summary>
+        public Guid RestaurantId { get; set; }
+
+        /// <summary>
+        /// The review's ID
+        /// </summary>
         public Guid Id { get; set; }
+
+        /// <summary>
+        /// The rating awarded to the restaurant.
+        ///
+        /// Note: this is pinned from a 0-5 scale; any ratings beyond that will be Max()/Min()'d to the appropriate scale.
+        /// </summary>
         public double Rating { get; set; }
+
+        /// <summary>
+        /// The name of the person who left the review
+        /// </summary>
         public string User { get; set; }
+
+        /// <summary>
+        /// The body of the review
+        /// </summary>
         public string ReviewText { get; set; }
     }
 }
--- MKEToolInterview.Models/RestaurantSummary.cs
+++ MKEToolInterview.Models/RestaurantSummary.cs
@@ -7,15 +7,35 @@ namespace MKEToolInterview.Models
 {
     public class RestaurantSummary
     {
+        /// <summary>
+        /// Primary Partition Key for the Restaurant
+        /// </summary>
         public Guid Id { get; set; }
+        /// <summary>
+        /// The restaurant's name
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// The restaurant's address. This is just a freeform text field for now; there is no separate handling of e.g. state or zip code
+        /// </summary>
         public string Address { get; set; }
+        /// <summary>
+        /// The restaurant's description
+        /// </summary>
         public string Description { get; set; }
 
         // TODO: as a first pass, this is just going to be a text field for hours, but this could probably be a more complex data structure
         // tied to DateTime(Offset?) in the future, to allow for better formatting and possibly timezone data.
+        /// <summary>
+        /// The hours the restaurant is open. This is just a freeform text field for now.
+        /// </summary>
         public string Hours { get; set; }
 
+        /// <summary>
+        /// The restaurant's average rating.
+        ///
+        /// Note: this is a calculated field, and should be treated as read-only. Any attempts to set this will be ignored.
+        /// </summary>
         public double? AverageRating { get; set; }
     }
 }
